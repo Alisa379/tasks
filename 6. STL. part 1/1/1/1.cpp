435402209a3937669e0d33f203b5cf11727a8c10
Alisa <alisamun2006@gmail.com>
2024-01-07 00:02:51 +1000

    commit

---
+++ 6. STL. part 1/1/1/1.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+void Bubble(std::vector<std::pair<char, int>> &vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size() - i - 1; j++) {
+            if (vec[j].second < vec[j + 1].second) {
+                std::swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::string string;
+    std::map<char, int> data;
+    std::getline(std::cin, string);
+
+    for (const auto& i : string) {
+        if (data.find(i) == data.end()) {
+            data[i] = 1;
+        }
+        else {
+            const std::map<char, int>::iterator& t = data.find(i);
+            t->second += 1;
+        } 
+    }
+
+    std::vector<std::pair<char, int>> vec;
+
+    for (const auto &i : data) {
+        vec.push_back(i);
+    }
+
+    Bubble(vec);
+
+    for (const auto& i : vec) {
+        std::cout << i.first << ": " << i.second << std::endl;
+    }
+}
+
---
+++ 6. STL. part 1/2/2/2.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+int main()
+{
+	std::set<int, std::greater<int>> s;
+	int amount;
+	std::cout << "[IN]:" << std::endl;
+	std::cin >> amount;
+	for (int i = 0; i < amount; ++i) {
+		int it;
+		std::cin >> it;
+		s.insert(it);
+	}
+	std::cout << "[OUT]:" << std::endl;
+	for (auto it : s) {
+		std::cout << it << std::endl;
+	}
+}
