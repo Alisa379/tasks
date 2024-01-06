2ee6b3b5087bd91b6c159f4532c70769e9cdb9e2
Alisa <alisamun2006@gmail.com>
2024-01-06 19:32:46 +1000

    commit

---
+++ MyProject/out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/informatics/libpqxx-7.8.1/test
+# Build directory: D:/c++/visual/6. Working with databases/5. Working with PostgreSQL from C++/MyProject/out/build/x64-Debug/libpqxx-build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runner "D:/c++/visual/6. Working with databases/5. Working with PostgreSQL from C++/MyProject/out/build/x64-Debug/libpqxx-build/test/runner.exe")
+set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "D:/informatics/libpqxx-7.8.1/test/CMakeLists.txt;12;add_test;D:/informatics/libpqxx-7.8.1/test/CMakeLists.txt;0;")
