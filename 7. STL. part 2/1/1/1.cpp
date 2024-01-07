5afd41a0147169a08ac4f5e5afc41669d04f5404
Alisa <alisamun2006@gmail.com>
2024-01-08 00:32:56 +1000

    commit

---
+++ 7. STL. part 2/1/1/1.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void un(T &V) {
+    sort(V.begin(), V.end());
+    auto new_end = unique(V.begin(), V.end());
+    V.erase(new_end, V.end());
+    std::for_each(V.begin(), V.end(), [](auto el) { std::cout << el << " "; });
+}
+
+int main()
+{
+    std::vector<int> V = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    un(V);
+}
+
---
+++ 7. STL. part 2/2/2/2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
+
+template<typename T>
+void print(T a) {
+    std::for_each(a.begin(), a.end(), [](auto el) { std::cout << el << ", "; });
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print(test_set);
+    print(test_list);
+    print(test_vector);
+}
+
