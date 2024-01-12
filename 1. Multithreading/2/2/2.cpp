0da19728a1413d86daa17da78ecf29623243ee84
Alisa <alisamun2006@gmail.com>
2024-01-12 22:40:53 +1000

    commit

--- 1. Multithreading/2/2/2.cpp
+++ 1. Multithreading/2/2/2.cpp
@@ -7,27 +7,32 @@
 using namespace std::chrono_literals;
 
 void sum(std::vector<int> v1, std::vector<int> v2, std::vector<int> v3, int ths, int th) {
-    size_t l = v1.size();
+    int l = 0;
+    for (auto el : v1) {
+        l++;
+    }
     int i = (l / ths) * th;
-    if (ths != th) {
+    if (ths - 1 != th) {
         int stop = (l / ths) * (th + 1);
-        for (i; i < stop; ++i) {
+        for (i; i < stop; i++) {
             v3.push_back(v1.at(i));
             v3.push_back(v2.at(i));
         }
     }
     else {
-        for (i; i < l; ++i) {
+        for (i; i < l; i++) {
             v3.push_back(v1.at(i));
             v3.push_back(v2.at(i));
         }
     }
 }
 
+
+
 int main()
 {
     std::cout << "Number of hardware cores: " << std::thread::hardware_concurrency() << std::endl << std::endl;;
-    std::cout << "        " << "\t" << "1000" << "\t" << "10000" << "\t" << "100000" << "\t" << "1000000" << std::endl;
+    std::cout << "        " << "\t" << "1000" << "\t" << "\t" << "10000" << "\t" << "\t"  << "100000" << "\t" << "\t" << "1000000" << std::endl;
     for (int i = 1; i < 17; i *= 2) {
         std::cout << i << " threads";
         for (int j = 1000; j <= 1000000; j *= 10) {
@@ -36,18 +41,16 @@ int main()
             std::vector<int> v1(j);
             std::vector<int> v2(j);
             std::vector<int> v3(j * 2);
-            for (int g = 0; g < i; ++g) {
-                v.push_back(std::thread(sum, v1, v2, v3, i, g - 1));
-                for (auto& el : v) {
-                    el.join();
-                }
+            for (int g = 0; g < i; g++) {
+                v.push_back(std::thread(sum, v1, v2, v3, i, g));
+            }
+            for (auto& el : v) {
+                el.join();
             }
             auto end = std::chrono::steady_clock::now();
-            auto diff = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+            auto diff = std::chrono::duration<double>(end - start);
             std::cout << "\t" << diff;
         }
         std::cout << std::endl;
     }
 }
-
-
