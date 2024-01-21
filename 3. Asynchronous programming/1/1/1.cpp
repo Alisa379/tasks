71e20bd8ceb966de922178c9d886910ddcb97288
Alisa <alisamun2006@gmail.com>
2024-01-21 20:41:04 +1000

    commit

---
+++ 3. Asynchronous programming/1/1/1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <future>
+#include <vector>
+#include <random>
+
+
+int k = 0;
+
+void find_min(std::vector<int> &vec, int i, std::promise<int> prom) {
+    int min = 10001;     
+    for (i ; i < 100; i++) {
+        if (vec[i] < min) {
+            min = vec[i];
+            k = i;
+        }
+    }
+    prom.set_value(min);
+}
+
+int main() {
+    std::vector<int> V(100);
+    for (auto it = V.begin(); it < V.end(); it++) {
+        *it = std::rand() % 1000;
+    }
+    for (int i = 0; i < 100; i++) {
+        std::promise<int> prom;
+        std::future<int> ft_res = prom.get_future();
+        auto ft = std::async(find_min, ref(V), i, std::move(prom));
+        int temp = V[i];
+        V[i] = ft_res.get();
+        V[k] = temp;
+    }
+    for (auto it : V) {
+        std::cout << it << " ";
+    }
+}
---
+++ 3. Asynchronous programming/2/2/2.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <future>
+
+template<typename It>
+void increment(It begin, It end) {
+    for (begin; begin < end; begin++) {
+        *begin += 1;
+    }
+}
+
+template<typename It, size_t size = 25>
+void for_each_(It begin, It end, void(&f)(It begin_, It end_)) {
+    size_t  curr_size = std::distance(begin, end);
+    if (curr_size <= size) {
+        f(begin, end);
+    }
+    auto mid = begin;
+    std::advance(mid, curr_size / 2);
+    auto ft_res = std::async(for_each_<It>, begin, mid, ref(f));
+    ft_res.get();
+    f(mid, end);
+}
+
+
+int main()
+{
+    std::vector<int> V(100, 2);
+    for_each_(V.begin(), V.end(), increment);
+    //increment(V.begin(), V.end());
+    for (auto it : V) {
+        std::cout << it << std::endl;
+    }
+}
+
